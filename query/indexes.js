557b8d1a3faa8f2423c08c5b8cb0277cc3f56a28
Federico Cantore <f.cantore85@gmail.com>
2026-01-21 13:23:19 +0100

    Update indexes.js

--- query/indexes.js
+++ query/indexes.js
@@ -1,1 +1,12 @@
 //Use this file to define indexes for your queries
+db.restaurants.createIndex({ borough: 1 })
+
+db.restaurants.createIndex({ "grades.score": 1 })
+
+db.restaurants.createIndex({ name: 1 });
+
+db.restaurants.createIndex({ cuisine: 1 });
+
+db.restaurants.createIndex({"location.coordinates": "2dsphere"})
+
+db.restaurants.createIndex({ "address.street": 1 });
